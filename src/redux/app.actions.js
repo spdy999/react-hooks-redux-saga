5e2ee9f01b5133f6b6562d7636a34986a298c185
spdy999 <1a1a1a.262626.0010@gmail.com>
2022-01-17 18:40:11 +0700

    create redux and saga actions

---
+++ src/redux/app.actions.js
@@ -0,0 +1,34 @@
+export const addTodos = (todo) => ({
+  type: 'ADD_TODO',
+  payload: todo,
+});
+
+export const deleteTodos = (id) => ({
+  type: 'DELETE_TODO',
+  payload: id,
+});
+
+export const updateTodos = (todo) => ({
+  type: 'UPDATE_TODO',
+  payload: todo,
+});
+
+export const addTodoSaga = (todo) => ({
+  type: 'ADD_TODO_SAGA',
+  payload: todo,
+});
+
+export const deleteTodoSaga = (todo) => ({
+  type: 'DELETE_TODO_SAGA',
+  payload: todo,
+});
+
+export const updateTodoSaga = (todo) => ({
+  type: 'UPDATE_TODO_SAGA',
+  payload: todo,
+});
+
+export const Todos = (todo) => ({
+  type: '_TODO',
+  payload: todo,
+});
