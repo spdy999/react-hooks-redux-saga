869994664e03d01c91cc1e107590b84bf29545c1
spdy999 <1a1a1a.262626.0010@gmail.com>
2022-01-17 18:39:43 +0700

    add mock data

---
+++ src/redux/data.js
@@ -0,0 +1,16 @@
+const todos = [
+  {
+    id: 0.51,
+    name: 'Apple',
+  },
+  {
+    id: 0.65,
+    name: 'Flower',
+  },
+  {
+    id: 0.32,
+    name: 'Chicken',
+  },
+];
+
+export default todos;
